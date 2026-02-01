fc5e58d8bccfab8bc67770965d24c066c5e0fb6f
bishesh-wl <bishesh@weblaunch.co.nz>
2026-02-02 10:30:17 +1300

    minor changes

--- TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bbc21122445950836b623bba8271c806c6170df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00516d6e36cfb8839c43f72df9a6663930f6d288")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
